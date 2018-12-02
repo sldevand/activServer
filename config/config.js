501256fc49e5adbbb21d786f5902b67c14d7c643
Sébastien Lorrain <sldeveloperand@gmail.com>
2018-12-02 14:44:05 +0100

    fix: added dirname to ini file path

--- config/config.js
+++ config/config.js
@@ -4,9 +4,9 @@ var fs = require('fs'),
 var iniFile;
 
 try {
-    iniFile = ini.parse(fs.readFileSync('./config/config.ini', 'utf-8'));
-} catch (error) {    
-    throw new Error("Error on ini file ----> "+error);
+    iniFile = ini.parse(fs.readFileSync(__dirname + '/config.ini', 'utf-8'));
+} catch (error) {
+    throw new Error("Error on ini file ----> " + error);
 }
 var isWin = /^win/.test(process.platform);
 var env = "LINUX";
