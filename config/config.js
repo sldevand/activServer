77511b716985b3f59a51881e6028110737db74b3
Sébastien Lorrain <sldeveloperand@gmail.com>
2018-12-02 14:34:32 +0100

    fix: try catch on ini file error

--- config/config.js
+++ config/config.js
@@ -1,18 +1,24 @@
 var fs = require('fs'),
     ini = require('ini');
 
-var iniFile = ini.parse(fs.readFileSync('./config/config.ini', 'utf-8'))
+var iniFile;
+
+try {
+    iniFile = ini.parse(fs.readFileSync('./config/config.ini', 'utf-8'));
+} catch (error) {    
+    throw new Error("Error on ini file ----> "+error);
+}
 var isWin = /^win/.test(process.platform);
 var env = "LINUX";
 if (isWin === true) {
     env = "WINDOWS"
 }
 
-var config =  {
-    portPath : iniFile[env].portPath,
-    rootPath : iniFile[env].rootPath,
-    logPath : iniFile[env].logPath,
-    sep : iniFile[env].sep
+var config = {
+    portPath: iniFile[env].portPath,
+    rootPath: iniFile[env].rootPath,
+    logPath: iniFile[env].logPath,
+    sep: iniFile[env].sep
 }
 
 module.exports = config;
