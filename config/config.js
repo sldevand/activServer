f2f583c0af59b7333affb7f49799b1e3ccfff9bb
Sébastien Lorrain <sldeveloperand@gmail.com>
2018-12-02 13:37:06 +0100

    fix: changed let to var (raspi node version 4.2)

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
-let fs = require('fs'),
+var fs = require('fs'),
     ini = require('ini');
 
-let iniFile = ini.parse(fs.readFileSync('./config/config.ini', 'utf-8'))
+var iniFile = ini.parse(fs.readFileSync('./config/config.ini', 'utf-8'))
 var isWin = /^win/.test(process.platform);
-let env = "LINUX";
+var env = "LINUX";
 if (isWin === true) {
     env = "WINDOWS"
 }
 
-let config =  {
+var config =  {
     portPath : iniFile[env].portPath,
     rootPath : iniFile[env].rootPath,
     logPath : iniFile[env].logPath,
