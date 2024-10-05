e1004519715db3bb5d0dd2d113c1c733ba0b5b10
sldevand <sldeveloperand@gmail.com>
2024-10-05 16:00:37 +0200

    rollback cors

--- server.js
+++ server.js
@@ -22,17 +22,7 @@ if (config.nodeEnv == 'development') {
 var server = http.createServer();
 var apiReq = new APIRequest(http, config.ip, '/' + config.apiUri + '/');
 var apiFetchReq = new APIFetchRequest('http://' + config.ip + '/' + config.apiUri);
-var io = require('socket.io')(server, {
-    origins: ["http://domusbox"],
-    handlePreflightRequest: (req, res) => {
-        res.writeHead(200, {
-            "Access-Control-Allow-Origin": "http://domusbox",
-            "Access-Control-Allow-Private-Network": true,
-            "Access-Control-Request-Private-Network": true
-        });
-        res.end();
-    }
-});
+var io = require('socket.io')(server);
 var logger = new Logger(apiFetchReq, df);
 
 resetScenariosStatuses();
